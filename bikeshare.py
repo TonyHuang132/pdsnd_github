e2499fe4a6322f0e7d492b44727c10fe43d8401d
Xiaozhou Huang <xiaozhouhuang.at.rice@gmail.com>
2021-12-08 01:26:06 -0500

    Change title

--- bikeshare.py
+++ bikeshare.py
@@ -36,7 +36,7 @@ def get_filters():
     """
 
     print('\n')
-    print_spin('Bikeshare Project', 10)
+    print_spin('Bikeshare ProjectX', 10)
     print('_(/)Bikeshare(/)______________________________')
     print(' Hello! Let\'s explore some US bikeshare data! '.center(78, '='))
     # get user input for city (chicago, new york city, washington).
